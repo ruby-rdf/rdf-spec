0decab61507b2b285abb032886b8bcad2eb2efb6
Arto Bendiken <arto@bendiken.net>
2010-02-04 01:28:23 +0100

    Imported RDF::Enumerable and RDF::Repository spec examples from RDF.rb 0.0.9.

---
+++ spec/enumerable_spec.rb
@@ -0,0 +1,12 @@
+require File.join(File.dirname(__FILE__), 'spec_helper')
+require 'rdf/spec/enumerable'
+
+describe RDF::Enumerable do
+  before :each do
+    @statements = RDF::NTriples::Reader.new(File.open("etc/doap.nt")).to_a
+    @enumerable = @statements.dup.extend(RDF::Enumerable)
+  end
+
+  # @see lib/rdf/spec/enumerable.rb
+  it_should_behave_like RDF_Enumerable
+end
