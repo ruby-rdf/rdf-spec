be90a5b33ac51c772540f1765af70be7964054ee
Arto Bendiken <arto@bendiken.net>
2010-02-04 01:21:09 +0100

    Added examples for RDF::Enumerable and RDF::Repository.

--- lib/rdf/spec.rb
+++ lib/rdf/spec.rb
@@ -14,6 +14,29 @@ module RDF
   #     config.include(RDF::Spec::Matchers)
   #   end
   #
+  # @example Using the shared examples for `RDF::Enumerable`
+  #   require 'rdf/spec/enumerable'
+  #   
+  #   describe RDF::Enumerable do
+  #     before :each do
+  #       @statements = RDF::NTriples::Reader.new(File.open("etc/doap.nt")).to_a
+  #       @enumerable = @statements.dup.extend(RDF::Enumerable)
+  #     end
+  #     
+  #     it_should_behave_like RDF_Enumerable
+  #   end
+  #
+  # @example Using the shared examples for `RDF::Repository`
+  #   require 'rdf/spec/repository'
+  #   
+  #   describe RDF::Repository do
+  #     before :each do
+  #       @repository = RDF::Repository.new
+  #     end
+  #     
+  #     it_should_behave_like RDF_Repository
+  #   end
+  #
   # @see http://rdf.rubyforge.org/
   # @see http://rspec.info/
   #
