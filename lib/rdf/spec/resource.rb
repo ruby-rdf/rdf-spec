53a711ff2b4e1ddc641bdad46c4a9c3f0930bfd6
Arto Bendiken <arto@bendiken.net>
2010-07-07 14:21:28 +0200

    Added shared spec stubs for RDF::Resource, RDF::Vocabulary, and RDF::Query.

---
+++ lib/rdf/spec/resource.rb
@@ -0,0 +1,8 @@
+require 'rdf/spec'
+require 'spec'
+
+share_as :RDF_Resource do
+  include RDF::Spec::Matchers
+
+  # TODO
+end
---
+++ lib/rdf/spec/vocab.rb
@@ -0,0 +1,8 @@
+require 'rdf/spec'
+require 'spec'
+
+share_as :RDF_Vocabulary do
+  include RDF::Spec::Matchers
+
+  # TODO
+end
