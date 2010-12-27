cf1cbf978d2d47c1332009f2216dfb142d64b10e
Arto Bendiken <arto@bendiken.net>
2010-12-27 14:14:07 +0100

    Configured an RSpec tag filter for marking specs as 1.8-only or 1.9-only.
    See: https://github.com/rspec/rspec-core/blob/master/Upgrade.markdown

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,4 +3,7 @@ require 'rdf/spec'
 
 Spec::Runner.configure do |config|
   config.include(RDF::Spec::Matchers)
+  config.exclusion_filter = {:ruby => lambda { |version|
+    RUBY_VERSION.to_s !~ /^#{version}/
+  }}
 end
