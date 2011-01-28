5312a28a8437e27a8875fdf4dcf34da115c6fd7a
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-01-27 18:13:06 -0800

    Start of generic Reader spec.

--- lib/rdf/spec/reader.rb
+++ lib/rdf/spec/reader.rb
@@ -3,5 +3,74 @@ require 'rdf/spec'
 share_as :RDF_Reader do
   include RDF::Spec::Matchers
 
-  # TODO
+  before(:each) do
+    raise '+@reader+ must be defined in a before(:each) block' unless instance_variable_get('@reader')
+    @reader_class = @reader.class
+  end
+
+  describe ".each" do
+    it "yields each reader" do
+      @reader_class.each do |r|
+        r.superclass.should == RDF::Reader
+      end
+    end
+  end
+
+  describe ".open" do
+    before(:each) do
+      RDF::Util::File.stub!(:open_file).and_return("foo")
+    end
+
+    it "yields reader given file_name" do
+      @reader_class.format.each do |f|
+        f.file_extensions.each_pair do |sym, content_type|
+          reader_mock = mock("reader")
+          reader_mock.should_receive(:got_here)
+          @reader_class.should_receive(:for).with(:file_name => "foo.#{sym}").and_return(@reader_class)
+          @reader_class.open("foo.#{sym}") do |r|
+            r.should be_a(RDF::Reader)
+            reader_mock.got_here
+          end
+        end
+      end
+    end
+
+    it "yields reader given symbol" do
+      @reader_class.format.each do |f|
+        sym = f.name.to_s.split('::')[-2].downcase.to_sym  # Like RDF::NTriples::Format => :ntriples
+        reader_mock = mock("reader")
+        reader_mock.should_receive(:got_here)
+        @reader_class.should_receive(:for).with(sym).and_return(@reader_class)
+        @reader_class.open("foo.#{sym}", :format => sym).should be_a(RDF::Reader)
+      end
+    end
+
+    it "yields reader given {:file_name => file_name}" do
+      @reader_class.format.each do |f|
+        f.file_extensions.each_pair do |sym, content_type|
+          reader_mock = mock("reader")
+          reader_mock.should_receive(:got_here)
+          @reader_class.should_receive(:for).with(:file_name => "foo.#{sym}").and_return(@reader_class)
+          @reader_class.open("foo.#{sym}", :file_name => "foo.#{sym}") do |r|
+            r.should be_a(RDF::Reader)
+            reader_mock.got_here
+          end
+        end
+      end
+    end
+
+    it "yields reader given {:content_type => 'a/b'}" do
+      @reader_class.format.each do |f|
+        f.content_types.each_pair do |content_type, formats|
+          reader_mock = mock("reader")
+          reader_mock.should_receive(:got_here)
+          @reader_class.should_receive(:for).with(:content_type => content_type).and_return(@reader_class)
+          @reader_class.open("foo.#{sym}", :content_type => content_type) do |r|
+            r.should be_a(RDF::Reader)
+            reader_mock.got_here
+          end
+        end
+      end
+    end
+  end
 end
