9c010cb855ff1c360f4fb5ea90695c049b1a9e47
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-01-27 17:25:53 -0800

    Format spec base, to be included in specs for things that subclass RDF::Format. Define @format_class as the sub-class to test.

--- lib/rdf/spec/format.rb
+++ lib/rdf/spec/format.rb
@@ -4,12 +4,12 @@ share_as :RDF_Format do
   include RDF::Spec::Matchers
 
   before(:each) do
-    raise '+@format+ must be defined in a before(:each) block' unless instance_variable_get('@format')
+    raise '+@format_class+ must be defined in a before(:each) block' unless instance_variable_get('@format_class')
   end
 
   describe ".each" do
     it "yields each format" do
-      @format.each do |f|
+      @format_class.each do |f|
         f.superclass.should == RDF::Format
       end
     end
@@ -17,7 +17,7 @@ share_as :RDF_Format do
 
   describe ".for" do
     it "detects format using file_name" do
-      @format.each do |f|
+      @format_class.each do |f|
         f.file_extensions.each_pair do |sym, content_type|
           RDF::Format.for("foo.#{sym}").should == f
         end
@@ -25,14 +25,14 @@ share_as :RDF_Format do
     end
 
     it "detects format using symbol" do
-      @format.each do |f|
+      @format_class.each do |f|
         sym = f.name.to_s.split('::')[-2].downcase.to_sym  # Like RDF::NTriples::Format => :ntriples
         RDF::Format.for(sym).should == f
       end
     end
 
     it "detects format using {:file_name => file_name}" do
-      @format.each do |f|
+      @format_class.each do |f|
         f.file_extensions.each_pair do |sym, content_type|
           RDF::Format.for(:file_name => "foo.#{sym}").should == f
         end
@@ -40,7 +40,7 @@ share_as :RDF_Format do
     end
 
     it "detects format using {:file_extension => ext}" do
-      @format.each do |f|
+      @format_class.each do |f|
         f.file_extensions.each_pair do |sym, content_type|
           RDF::Format.for(:file_extension => sym).should == f
         end
@@ -48,7 +48,7 @@ share_as :RDF_Format do
     end
 
     it "detects format using {:content_type => 'a/b'}" do
-      @format.each do |f|
+      @format_class.each do |f|
         f.content_types.each_pair do |content_type, formats|
           format = RDF::Format.for(:content_type => content_type)
           formats.first.should include(format)
@@ -60,7 +60,7 @@ share_as :RDF_Format do
   
   describe ".reader" do
     it "returns a reader" do
-      @format.each do |f|
+      @format_class.each do |f|
         f.reader.superclass.should == RDF::Reader
       end
     end
@@ -68,7 +68,7 @@ share_as :RDF_Format do
   
   describe ".writer" do
     it "returns a writer" do
-      @format.each do |f|
+      @format_class.each do |f|
         f.writer.superclass.should == RDF::Writer
       end
     end
