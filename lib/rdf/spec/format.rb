14dcb852a52b5e279dde5606182a1d0a0e29e558
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-01-27 17:22:51 -0800

    Format spec base, to be included in specs for things that subclass RDF::Format. Define @format as the sub-class to test.

--- lib/rdf/spec/format.rb
+++ lib/rdf/spec/format.rb
@@ -3,5 +3,74 @@ require 'rdf/spec'
 share_as :RDF_Format do
   include RDF::Spec::Matchers
 
-  # TODO
+  before(:each) do
+    raise '+@format+ must be defined in a before(:each) block' unless instance_variable_get('@format')
+  end
+
+  describe ".each" do
+    it "yields each format" do
+      @format.each do |f|
+        f.superclass.should == RDF::Format
+      end
+    end
+  end
+
+  describe ".for" do
+    it "detects format using file_name" do
+      @format.each do |f|
+        f.file_extensions.each_pair do |sym, content_type|
+          RDF::Format.for("foo.#{sym}").should == f
+        end
+      end
+    end
+
+    it "detects format using symbol" do
+      @format.each do |f|
+        sym = f.name.to_s.split('::')[-2].downcase.to_sym  # Like RDF::NTriples::Format => :ntriples
+        RDF::Format.for(sym).should == f
+      end
+    end
+
+    it "detects format using {:file_name => file_name}" do
+      @format.each do |f|
+        f.file_extensions.each_pair do |sym, content_type|
+          RDF::Format.for(:file_name => "foo.#{sym}").should == f
+        end
+      end
+    end
+
+    it "detects format using {:file_extension => ext}" do
+      @format.each do |f|
+        f.file_extensions.each_pair do |sym, content_type|
+          RDF::Format.for(:file_extension => sym).should == f
+        end
+      end
+    end
+
+    it "detects format using {:content_type => 'a/b'}" do
+      @format.each do |f|
+        f.content_types.each_pair do |content_type, formats|
+          format = RDF::Format.for(:content_type => content_type)
+          formats.first.should include(format)
+          formats.first.should include(f)
+        end
+      end
+    end
+  end
+  
+  describe ".reader" do
+    it "returns a reader" do
+      @format.each do |f|
+        f.reader.superclass.should == RDF::Reader
+      end
+    end
+  end
+  
+  describe ".writer" do
+    it "returns a writer" do
+      @format.each do |f|
+        f.writer.superclass.should == RDF::Writer
+      end
+    end
+  end
 end
