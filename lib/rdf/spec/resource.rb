48d898af860f4d8c7499c50e9a599bacb8b1b29e
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-09-06 18:32:16 -0700

    Move list, node, resource, statement, term, uri and value specs to RDF.rb

--- lib/rdf/spec/resource.rb
+++
@@ -1,7 +0,0 @@
-require 'rdf/spec'
-
-share_as :RDF_Resource do
-  include RDF::Spec::Matchers
-
-  # TODO
-end
--- lib/rdf/spec/term.rb
+++
@@ -1,7 +0,0 @@
-require 'rdf/spec'
-
-share_as :RDF_Term do
-  include RDF::Spec::Matchers
-
-  # TODO
-end
