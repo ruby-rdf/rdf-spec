e197bc56c3648a345d1f0ccec2ee5b5c0ae321ed
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-09-11 22:11:40 -0700

    Format.for specs.

--- lib/rdf/spec/format.rb
+++ lib/rdf/spec/format.rb
@@ -6,18 +6,6 @@ share_as :RDF_Format do
   before(:each) do
     raise raise '+@format_class+ must be defined in a before(:each) block' unless instance_variable_get('@format_class')
   end
-
-  describe ".for" do
-    it "detects format using file_name" do
-      RDF::Format.each do |f|
-        f.file_extensions.each_pair do |sym, content_type|
-          RDF::Format.for("foo.#{sym}").should == RDF::Format.for(:file_name => "foo.#{sym}")
-          RDF::Format.for("foo.#{sym}").should == RDF::Format.for(:file_extension => sym)
-          RDF::Format.for("foo.#{sym}").should == RDF::Format.for(:content_type => content_type)
-        end
-      end
-    end
-  end
   
   describe ".reader" do
     it "returns a reader" do
