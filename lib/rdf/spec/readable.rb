884e0acbb248fffae36926cbaafdd154ac885274
Gregg Kellogg <gregg@kellogg-assoc.com>
2012-02-07 17:36:06 -0800

    Simple Readable tests.

--- lib/rdf/spec/readable.rb
+++ lib/rdf/spec/readable.rb
@@ -3,5 +3,15 @@ require 'rdf/spec'
 share_as :RDF_Readable do
   include RDF::Spec::Matchers
 
-  # TODO
+  before :each do
+    raise '+@readable+ must be defined in a before(:each) block' unless instance_variable_get('@readable')
+  end
+  
+  it "responds to #readable?" do
+    @readable.respond_to?(:readable?)
+  end
+  
+  it "implements #readable?" do
+    !!@readable.readable?.should == @readable.readable?
+  end
 end
