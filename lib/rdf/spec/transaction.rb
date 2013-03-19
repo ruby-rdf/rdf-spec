4e86dabf9a12eb901a1a97aa28b6bb8192df61ef
Gregg Kellogg <gregg@kellogg-assoc.com>
2013-03-18 17:29:53 -0700

    Created Transaction tests.

--- lib/rdf/spec/transaction.rb
+++ lib/rdf/spec/transaction.rb
@@ -1,62 +1,111 @@
 require 'rdf/spec'
 
-module RDF_Transaction
-  extend RSpec::SharedContext
+# Pass in an instance of RDF::Transaction as follows:
+#
+#   it_behaves_like "RDF_Transaction", RDF::Transaction
+shared_examples "RDF_Transaction" do |klass|
   include RDF::Spec::Matchers
 
-  before :each do
-    raise '+@transaction+ must be defined in a before(:each) block' unless instance_variable_get('@transaction')
-  end
-
   describe RDF::Transaction do
-    #TODO
+    subject {klass.new(:context => RDF::Graph.new, :insert => RDF::Graph.new, :delete => RDF::Graph.new)}
+
     describe "#initialize" do
-      it "accepts a graph"
-      it "accepts a context"
-      it "accepts inserts"
-      it "accepts deletes"
+      subject {klass}
+      it "accepts a graph" do
+        g = mock("graph")
+        this = subject.new(:graph => g)
+        this.graph.should == g
+      end
+
+      it "accepts a context" do
+        c = mock("context")
+        this = subject.new(:graph => c)
+        this.graph.should == c
+        this.context.should == c
+
+        this = subject.new(:context => c)
+        this.graph.should == c
+        this.context.should == c
+      end
+
+      it "accepts inserts" do
+        g = mock("inserts")
+        this = subject.new(:insert => g)
+        this.inserts.should == g
+      end
+
+      it "accepts deletes" do
+        g = mock("deletes")
+        this = subject.new(:delete => g)
+        this.deletes.should == g
+      end
     end
+
+    its(:deletes) {should be_a(RDF::Enumerable)}
+    its(:inserts) {should be_a(RDF::Enumerable)}
+    it {should be_mutable}
+    it {should_not be_readable}
+
   
-    describe "#graph" do
-      it "is mutable"
+    it "does not respond to #load" do
+      lambda {subject.load("http://example/")}.should raise_error(NoMethodError)
     end
-  
-    describe "#deletes" do
-      it "is mutable"
+
+    it "does not respond to #update" do
+      lambda {subject.update(RDF::Statement.new)}.should raise_error(NoMethodError)
     end
-  
-    describe "#inserts" do
-      it "is mutable"
+
+    it "does not respond to #clear" do
+      lambda {subject.clear}.should raise_error(NoMethodError)
     end
-  
-    describe "#readable?" do
-      it "returns false" do
-        @transaction.readable?.should be_false
+
+    describe "#execute" do
+      let(:s) {RDF::Statement.new(RDF::URI("s"), RDF::URI("p"), RDF::URI("o"))}
+      let(:r) {mock("repository")}
+
+      it "deletes statements" do
+        r.should_receive(:delete).with(s)
+        r.should_not_receive(:insert)
+        subject.delete(s)
+        subject.execute(r)
+      end
+
+      it "inserts statements" do
+        r.should_not_receive(:delete)
+        r.should_receive(:insert).with(s)
+        subject.insert(s)
+        subject.execute(r)
+      end
+
+      it "calls before_execute" do
+        subject.should_receive(:before_execute).with(r, {})
+        subject.execute(r)
+      end
+
+      it "calls after_execute" do
+        subject.should_receive(:after_execute).with(r, {})
+        subject.execute(r)
+      end
+
+      it "returns self" do
+        subject.execute(r).should == subject
       end
     end
   
-    describe "#execute" do
-      it "deletes statements"
-      it "inserts statements"
-      it "calls before_execute"
-      it "calls after_execute"
-      it "returns self"
-      it "does not delete statements on failures"
-      it "does not insert statements on failures"
-    end
-  
     describe "#delete_statement" do
-      it "adds statement to #deletes"
-      it "does not remove statement from graph"
+      let(:s) {RDF::Statement.new(RDF::URI("s"), RDF::URI("p"), RDF::URI("o"))}
+      it "adds statement to #deletes" do
+        subject.delete(s)
+        subject.deletes.to_a.should == [s]
+      end
     end
   
     describe "#insert_statement" do
-      it "adds statement to #inserts"
-      it "does not add statement to graph"
+      let(:s) {RDF::Statement.new(RDF::URI("s"), RDF::URI("p"), RDF::URI("o"))}
+      it "adds statement to #inserts" do
+        subject.insert(s)
+        subject.inserts.to_a.should == [s]
+      end
     end
-  
-    it "does not respond to #load"
-    it "does not respond to #update"
-    it "does not respond to #clear"
   end
 end
