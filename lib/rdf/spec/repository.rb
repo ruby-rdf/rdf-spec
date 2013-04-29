b5e176ff1acf32b3751117e77f2b6d6bf0dc8a8b
Gregg Kellogg <gregg@kellogg-assoc.com>
2013-04-28 17:48:00 -0700

    From repository spec, conditionally do mutable and durable only if the repository is mutable.

--- lib/rdf/spec/repository.rb
+++ lib/rdf/spec/repository.rb
@@ -7,64 +7,46 @@ module RDF_Repository
   before :each do
     raise '+@repository+ must be defined in a before(:each) block' unless instance_variable_get('@repository')
     @statements = RDF::Spec.quads
+    if @repository.empty? && @repository.writable?
+      @repository.insert(*@statements)
+    elsif @repository.empty?
+      raise "+@repository+ must respond to #<< or be pre-populated with the statements in #{RDF::Spec::TRIPLES_FILE} in a before(:each) block"
+    end
+    @countable = @repository
     @enumerable = @repository
+    @queryable = @repository
+    @mutable = @repository
   end
 
   describe RDF::Repository do
     context "when counting statements" do
       require 'rdf/spec/countable'
-
-      before :each do
-        if @repository.empty? && @repository.writable?
-          @repository.insert(*@statements)
-        elsif @repository.empty?
-          raise "+@repository+ must respond to #<< or be pre-populated with the statements in #{RDF::Spec::TRIPLES_FILE} in a before(:each) block"
-        end
-        @countable = @repository
-      end
-
       include RDF_Countable
     end
 
     context "when enumerating statements" do
       require 'rdf/spec/enumerable'
-
-      before :each do
-        if @repository.empty? && @repository.writable?
-          @repository.insert(*@statements)
-        elsif @repository.empty?
-          raise "+@repository+ must respond to #<< or be pre-populated with the statements in #{RDF::Spec::TRIPLES_FILE} in a before(:each) block"
-        end
-        @enumerable = @repository
-      end
-
       include RDF_Enumerable
     end
 
     context "when querying statements" do
       require 'rdf/spec/queryable'
-
-      before :each do
-        @queryable = @repository
-      end
-
       include RDF_Queryable
     end
 
-    context "when updating" do
+    context "when updating", :if => lambda {@mutable.mutable?} do
       require 'rdf/spec/mutable'
-
-      before :each do
-        @mutable = @repository
+      before(:each) do
+        @mutable.clear
       end
-
       include RDF_Mutable
     end
 
-    context "as a durable repository" do
+    context "as a durable repository", :if => lambda {@repository.mutable?} do
       require 'rdf/spec/durable'
 
       before :each do
+        @repository.clear
         @load_durable ||= lambda { @repository }
       end
 
