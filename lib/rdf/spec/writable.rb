ac1b1dd1832e9da9ee0bdef0d7e14586d5097c7a
Gregg Kellogg <gregg@greggkellogg.net>
2015-10-29 16:17:47 -0700

    Expect inserting an incomplete statement to raise ArgumentError. For ruby-rdf/rdf/issues#226.

--- lib/rdf/spec/writable.rb
+++ lib/rdf/spec/writable.rb
@@ -107,9 +107,9 @@ RSpec.shared_examples 'an RDF::Writable' do
 
     it "should not insert an incomplete statement" do
       if subject.writable?
-        subject.insert(RDF::Statement.from(statement.to_hash.merge(subject: nil)))
-        subject.insert(RDF::Statement.from(statement.to_hash.merge(predicate: nil)))
-        subject.insert(RDF::Statement.from(statement.to_hash.merge(object: nil)))
+        expect {subject.insert(RDF::Statement.from(statement.to_hash.merge(subject: nil)))}.to raise_error(ArgumentError)
+        expect {subject.insert(RDF::Statement.from(statement.to_hash.merge(predicate: nil)))}.to raise_error(ArgumentError)
+        expect {subject.insert(RDF::Statement.from(statement.to_hash.merge(object: nil)))}.to raise_error(ArgumentError)
         expect(subject.count).to eql 0
       end
     end
