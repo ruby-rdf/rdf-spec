986d8a3f0630c8292fea6b0d9687118d416c73a8
Tom Johnson <tom@dp.la>
2016-01-12 16:42:38 -0800

    Soften RDF::Transaction buffering requirements
    Not every `Transaction` needs to keep an up-to-date `Changeset` in
    buffer. This change loosens the requirements. some of the tests are
    removed to the `rdf` test suite in `spec/transaction_spec.rb`.

--- lib/rdf/spec/transaction.rb
+++ lib/rdf/spec/transaction.rb
@@ -43,11 +43,6 @@ shared_examples "an RDF::Transaction" do |klass|
     it 'is false when changeset is empty' do
       expect(subject).not_to be_buffered
     end
-
-    it 'is true if changeset has changes' do
-      subject.insert([:s, :p, :o])
-      expect(subject).to be_buffered
-    end
   end
 
   describe '#changes' do
@@ -64,23 +59,33 @@ shared_examples "an RDF::Transaction" do |klass|
     let(:st) { RDF::Statement(:s, RDF::URI('p'), 'o') }
     
     it 'adds to deletes' do
-      expect { subject.delete(st) }
-        .to change { subject.changes.deletes }.to contain_exactly(st)
+      subject.repository.insert(st)
+
+      expect do 
+        subject.delete(st)
+        subject.execute
+      end.to change { subject.repository.empty? }.from(false).to(true)
     end
 
     it 'adds multiple to deletes' do
       sts = [st] << RDF::Statement(:x, RDF::URI('y'), 'z')
+      subject.repository.insert(*sts)
 
-      expect { subject.delete(*sts) }
-        .to change { subject.changes.deletes }.to contain_exactly(*sts)
+      expect do
+        subject.delete(*sts)
+        subject.execute
+      end.to change { subject.repository.empty? }.from(false).to(true)
     end
 
     it 'adds enumerable to deletes' do
       sts = [st] << RDF::Statement(:x, RDF::URI('y'), 'z')
       sts.extend(RDF::Enumerable)
+      subject.repository.insert(sts)
 
-      expect { subject.delete(sts) }
-        .to change { subject.changes.deletes }.to contain_exactly(*sts)
+      expect do
+        subject.delete(sts)
+        subject.execute
+      end.to change { subject.repository.empty? }.from(false).to(true)
     end
   end
 
@@ -88,23 +93,32 @@ shared_examples "an RDF::Transaction" do |klass|
     let(:st) { RDF::Statement(:s, RDF::URI('p'), 'o') }
     
     it 'adds to inserts' do
-      expect { subject.insert(st) }
-        .to change { subject.changes.inserts }.to contain_exactly(st)
+      expect do
+        subject.insert(st)
+        subject.execute
+      end.to change { subject.repository.statements }
+              .to contain_exactly(st)
     end
 
     it 'adds multiple to inserts' do
       sts = [st] << RDF::Statement(:x, RDF::URI('y'), 'z')
       
-      expect { subject.insert(*sts) }
-        .to change { subject.changes.inserts }.to contain_exactly(*sts)
+      expect do
+        subject.insert(*sts)
+        subject.execute
+      end.to change { subject.repository.statements }
+              .to contain_exactly(*sts)
     end
 
     it 'adds enumerable to inserts' do
       sts = [st] << RDF::Statement(:x, RDF::URI('y'), 'z')
       sts.extend(RDF::Enumerable)
 
-      expect { subject.insert(sts) }
-        .to change { subject.changes.inserts }.to contain_exactly(*sts)
+      expect do
+        subject.insert(sts)
+        subject.execute
+      end.to change { subject.repository.statements }
+              .to contain_exactly(*sts)
     end
   end
 
