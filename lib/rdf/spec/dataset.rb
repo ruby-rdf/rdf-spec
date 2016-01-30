46eb4c90774cd0ed2a635e4bcf0d53efa1e5456c
Tom Johnson <tom@dp.la>
2016-01-30 10:37:20 -0800

    Introduce `RDF::Dataset` as a distinct concept
    `Dataset` is introduced as a base class of `RDF::Repository`. It
    represents a (default read only) set of graphs. Some of the specs
    are removed from the repository specs, but are re-included indirectly
    through dataset's tests.

---
+++ lib/rdf/spec/dataset.rb
@@ -0,0 +1,40 @@
+require 'rdf/spec'
+
+RSpec.shared_examples 'an RDF::Dataset' do
+  include RDF::Spec::Matchers
+
+  before :each do
+    raise 'dataset must be set with `let(:dataset)' unless
+      defined? dataset
+
+    if repository.empty?
+      raise "+dataset+ must respond be pre-populated with the statements in #{RDF::Spec::TRIPLES_FILE} in a before block"
+    end
+  end
+
+  let(:countable)  { dataset }
+  let(:enumerable) { dataset }
+  let(:queryable)  { dataset }
+
+  context "when counting statements" do
+    require 'rdf/spec/countable'
+    it_behaves_like 'an RDF::Countable'
+  end
+
+  context "when enumerating statements" do
+    require 'rdf/spec/enumerable'
+    it_behaves_like 'an RDF::Enumerable'
+  end
+
+  context "when querying statements" do
+    require 'rdf/spec/queryable'
+    it_behaves_like 'an RDF::Queryable'
+  end
+
+  describe '#isolation_level' do
+    it 'is an allowable isolation level' do
+      expect(described_class::ISOLATION_LEVELS)
+        .to include(subject.isolation_level)
+    end
+  end
+end
--- lib/rdf/spec/repository.rb
+++ lib/rdf/spec/repository.rb
@@ -15,36 +15,21 @@ RSpec.shared_examples 'an RDF::Repository' do
     end
   end
 
-  let(:countable) { repository }
-  let(:enumerable) { repository }
-  let(:queryable) { repository }
   let(:mutable) { repository }
+  let(:dataset) { repository }
 
-  context "when counting statements" do
-    require 'rdf/spec/countable'
-    it_behaves_like 'an RDF::Countable'
+  context 'as dataset' do
+    require 'rdf/spec/dataset'
+    it_behaves_like 'an RDF::Dataset'
   end
-
-  context "when enumerating statements" do
-    require 'rdf/spec/enumerable'
-    it_behaves_like 'an RDF::Enumerable'
-  end
-
-  context "when querying statements" do
-    require 'rdf/spec/queryable'
-    it_behaves_like 'an RDF::Queryable'
-  end
-
-  # FIXME: This should be condition on the repository being mutable
+  
   context "when updating" do
     require 'rdf/spec/mutable'
 
     before { mutable.clear }
-
     it_behaves_like 'an RDF::Mutable'
   end
  
-  # FIXME: This should be condition on the repository being mutable
   context "as a durable repository" do
     require 'rdf/spec/durable'
 
